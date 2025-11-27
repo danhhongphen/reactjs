9854445bf19d4f54b916f6c7b00a4d63d1e8e415
danhhongphen <primarydemon@gmail.com>
2025-11-27 10:39:28 +0700

    css

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,12 @@
+import "./style.css"
+
+const MyComponent = () => {
+    return (
+        <>
+            <div className="phanh" style={{ borderRadius: "50px" }}>Phanh & Thuy</div>
+            <div className="abc">abc</div>
+        </>
+    );
+}
+
+export default MyComponent;
