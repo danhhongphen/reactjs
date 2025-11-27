3d42c897cbf3e847f9a8756524059149789eb04d
danhhongphen <primarydemon@gmail.com>
2025-11-27 16:49:46 +0700

    handle on change

--- src/components/todo/ToDoNew.jsx
+++ src/components/todo/ToDoNew.jsx
@@ -1,12 +1,24 @@
 
 const ToDoNew = (props) => {
     const addNewToDoFunc = props.addNewToDoFunc;
+    const handleClick = () => {
+        alert("click");
+    }
+
+    const handleOnChange = (text) => {
+        console.log(text)
+    }
 
     addNewToDoFunc("cam");
     return (
         <div className="todo-new">
-            <input type="text"></input>
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => { handleOnChange(event.target.value) }}
+            ></input>
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     );
 }
